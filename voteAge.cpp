f864daf930db617cf74d5394b7256035c58342f5
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-09-26 14:37:53 +0400

    Create voteAge.cpp

---
+++ voteAge.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int age;
+
+    cout << "PLEASE ENTER YOUR AGE: ";
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "You are eligible to vote" << endl;
+    }
+
+    else {
+        cout << "You are not eligible to vote" << endl;
+    }
+
+    cin.get();
+}
