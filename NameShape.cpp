5c35289d21df4c349be3f5b05d8ef1c6b77face5
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-09-26 14:48:49 +0400

    Create NameShape.cpp

---
+++ NameShape.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int sides;
+    cout << "PLEASE ENTER THE NUMBER OF SIDES OF THE SHAPE: (MAX OF 10 ";
+    cin >> sides;
+
+    if (sides == 3) {
+        cout << "The Shape is a TRIANGLE" << endl;
+    }
+
+    else if (sides == 4) {
+        cout << "The Shape is a RECTANGLE" << endl;
+    }
+
+    else if (sides == 5) {
+        cout << "The Shape is a PENTAGON" << endl;
+    }
+
+    else if (sides == 6) {
+        cout << "The Shape is a HEXAGON" << endl;
+    }
+
+    else if (sides == 7) {
+        cout << "The Shape is a HEPTAGON" << endl;
+    }
+
+    else if (sides == 8) {
+        cout << "The Shape is an OCTAGON" << endl;
+    }
+
+    else if (sides == 9) {
+        cout << "The Shape is a NONAGON" << endl;
+    }
+
+    else if (sides == 10) {
+        cout << "The Shape is a DECAGON" << endl;
+    }
+
+    else {
+        cout << "ERROR: INVALID INPUT\nPLEASE ENTER A SHAPE WITH LESS THAN 10 SIDES" << endl;
+    }
+}
+  
