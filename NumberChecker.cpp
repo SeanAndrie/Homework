f9bdd2ca971a03c0bfa65090b7d6980d8e7ff6f8
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-09-26 14:46:18 +0400

    Create NumberChecker.cpp

---
+++ NumberChecker.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int num;
+      cin >> num;
+
+      if (num >= 0) {
+          cout << num << " is a postive number" << endl;
+      }
+
+      else {
+          cout << num << " is a negative number" << endl;
+      }
+}
