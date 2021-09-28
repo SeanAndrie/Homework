dfe0a968d7333d57b5f714321b56c1575a2657fa
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-09-28 14:51:46 +0400

    Update MarkMyWords(Slide16).cpp

--- MarkMyWords(Slide16).cpp
+++ MarkMyWords(Slide16).cpp
@@ -1,23 +1,28 @@
-int grade;
-    cout << "PLEASE ENTER YOUR GRADE: " << endl;
-    cin >> grade;
+#include <ioststream>
+using namespace std;
+int main()
+{
+    int grade;
+        cout << "PLEASE ENTER YOUR GRADE: " << endl;
+        cin >> grade;
 
-    if (grade >= 70) {
-        cout << "GRADE: A" << endl;
-    }
+        if (grade >= 70) {
+            cout << "GRADE: A" << endl;
+        }
 
-    else if (grade >= 60 && grade < 70) {
-        cout << "GRADE: B" << endl;
-    }
+        else if (grade >= 60 && grade < 70) {
+            cout << "GRADE: B" << endl;
+        }
 
-    else if (grade >= 50 && grade < 60) {
-        cout << "GRADE: C" << endl;
-    }
+        else if (grade >= 50 && grade < 60) {
+            cout << "GRADE: C" << endl;
+        }
 
-    else if (grade >= 40 && grade < 50) {
-        cout << "GRADE: D" << endl;
-    }
+        else if (grade >= 40 && grade < 50) {
+            cout << "GRADE: D" << endl;
+        }
 
-    else {
-        cout << "GRADE: F" << endl;
-    }
+        else {
+            cout << "GRADE: F" << endl;
+        }
+}
