ba68285054ebbda38e8ba89178dcbbaf20345311
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-09-28 14:47:15 +0400

    Create MarkMyWords(Slide16).cpp

---
+++ MarkMyWords(Slide16).cpp
@@ -0,0 +1,23 @@
+int grade;
+    cout << "PLEASE ENTER YOUR GRADE: " << endl;
+    cin >> grade;
+
+    if (grade >= 70) {
+        cout << "GRADE: A" << endl;
+    }
+
+    else if (grade >= 60 && grade < 70) {
+        cout << "GRADE: B" << endl;
+    }
+
+    else if (grade >= 50 && grade < 60) {
+        cout << "GRADE: C" << endl;
+    }
+
+    else if (grade >= 40 && grade < 50) {
+        cout << "GRADE: D" << endl;
+    }
+
+    else {
+        cout << "GRADE: F" << endl;
+    }
