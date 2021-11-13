b21c428db8ca4bf90dd475e2db608ee813c49a9a
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-11-14 02:37:57 +0400

    Create SomeCounting(Lecture 10).cpp

---
+++ SomeCounting(Lecture 10).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+  /* 0 t0 50 in increments of 1
+    for (int i = 0; i <= 50; i++) {
+        cout << i << endl;
+    }*/
+
+    /* 50 to 0 in incremements of 1
+    for (int i = 50; i >= 0; i--) {
+        cout << i << endl;
+    }*/
+
+    /* 30 to 50 in increments of 1
+    for (int i = 30; i <= 50; i++) {
+        cout << i << endl;
+    }*/
+
+    /* 50 to 10 in increments of 2
+    for (int i = 50; i >= 10; i--) {
+        cout << i << endl;
+        i = i - 1;
+    }*/
+
+    /* 100 to 200 in increments of 5
+    for (int i = 100; i <= 200; i++) {
+        cout << i << endl;
+        i = i + 4;
+    }*/
+}
