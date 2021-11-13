2a86611f37839de58296430aa2c579271b2b05e2
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-11-14 02:34:50 +0400

    Create Countdown.cpp

---
+++ Countdown.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   for (int i = 10; i >= 0; i--) {
+        cout << i << endl;
+
+        if (i == 0) {
+            cout << "WE HAVE LIFT OFF!" << endl;
+        }
+    }
+}
