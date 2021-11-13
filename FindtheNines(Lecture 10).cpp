44b95c37d1df158cedb1835376bd1ecb4e75375b
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-11-14 02:48:23 +0400

    Create FindtheNines(Lecture 10).cpp

---
+++ FindtheNines(Lecture 10).cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std;;
+
+int main(){
+  
+  int sum = 0;
+  
+  cout << "NUMBERS DIVISIBLE BY 9: ";
+  
+  for (int i = 100; i <= 200; i++) {
+    if ( i % 9 == 0 ) {
+      cout << i << "; ";
+    }
+  }
+  cout << endl;
+  cout << "SUM: " << sum << endl;
+}
