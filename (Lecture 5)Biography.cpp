72b3fe6d1b6d9dc28268320cb6cc9023e4ab6725
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-12-19 21:12:49 +0400

    Create (Lecture 5)Biography.cpp

---
+++ (Lecture 5)Biography.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name, home, age;
+    cout << "PLEASE ENTER YOUR NAME: ";
+    cin >> name;
+    cout << "\nPLEASE ENTER YOUR AGE: ";
+    cin >> age;
+    cout << "\nPLEASE ENTER YOUR HOMETOWN: ";
+    cin >> home;
+    
+    cout << "\n---------------" << endl;
+    cout << "NAME: " << name << endl;
+    cout << "AGE: " << age << endl;
+    cout << "HOMETOWN: " << home << endl;
+    cout << "----------------" << endl;
+}
