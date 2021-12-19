bbd312eb31e7dc8321a02f0376c8a355b584e564
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-12-19 20:54:04 +0400

    Create (Lecture 4)PossibleDataTypes.cpp

---
+++ (Lecture 4)PossibleDataTypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int firstNumber = 1;
+    bool iCanCode = true;
+    char hopefulGrade = 'a';
+    double myDecimal = 1.0;
+    string minimalSentance= "y";
+    int keyMash= 13213123;
+    float mysteryDataType = 5.6f;
+    
+    cin.get(); //keeps console window open in Visual Studio
+    return 0;
+    
+}
