d6f5b07345a953608f954bcac5860efe8ce8e0b9
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-12-19 21:00:39 +0400

    Create (Lecture 4)UntidyCode.cpp

---
+++ (Lecture 4)UntidyCode.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 6;
+    cout << "This is untidy code. I'm surprised it works." << endl;
+    cout << "It has " << number << " lines of code - each more hideous than the last." << endl;
+    cout << "You should probably add some line breaks in the text too." << endl;
+    cin.get();
+    return 0;
+}
