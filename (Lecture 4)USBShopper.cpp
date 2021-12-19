192e52d0d57eb42145e980a82ebbbfc4b182589c
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-12-19 21:06:05 +0400

    Create (Lecture 4)USBShopper.cpp

---
+++ (Lecture 4)USBShopper.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int total = 50, usb = 6, calc, left;
+    
+    calc = 50 / 6;
+    left = 50 % 6;
+    
+    cout << "With 5O AED, the girl can buy " << calc << " USB sticks. She will be left with " << left << " fils." << endl;
+}
