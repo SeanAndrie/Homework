c64eb6f01c5ca6ec74dfb6f4a3fe481fa14f40e6
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-12-19 21:23:51 +0400

    Update (Lecture 5)Temperature.cpp

--- (Lecture 5)Temperature.cpp
+++ (Lecture 5)Temperature.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
-    int fahren, calc;
+    #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Fahrenheit to Celsius
+    /*int fahren, calc;
     cout << "-FAHRENHEIT TO CELSIUS-" << endl;
     cout << "\nPLEASE ENTER THE TEMP. IN FAHRENHEIT: ";
     cin >> fahren;
@@ -12,6 +19,16 @@ int main()
     calc = (fahren - 32) * 5/9;
     
     cout << endl;
-    cout << fahren << "F IS " << calc << "C" << endl;
+    cout << fahren << "F IS " << calc << "C" << endl;*/
     
+    //Celsius to Fahrenheit
+    /*int cels, calc;
+    cout << "-CELSIUS TO FAHRENHEIT-" << endl;
+    cout << "\nPLEASE ENTER THE TEMP. IN CELSIUS: ";
+    cin >> cels;
+    
+    calc = (cels * 9/5) + 32;
+    
+    cout << endl;
+    cout << cels << "C IS " << calc << "F" << endl;*/
 }
