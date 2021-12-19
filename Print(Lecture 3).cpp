41b4c225735e5c1b78c9b80f8377d0908318602f
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-12-19 20:50:01 +0400

    Create Print(Lecture 3).cpp

---
+++ Print(Lecture 3).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name, age, add, permadd;
+    
+    name = "Sean Gadingan";
+    age = "18";
+    add = "Al Ma'ani Street, Mushrif, Abu Dhabi";
+    permadd = "Taurus Street, Marilao, Bulacan, Philippines";
+    
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Current Addres: " << add << endl;
+    cout << "Permanent Address: " << permadd << endl;
+}
