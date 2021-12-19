29c87c640a4d89db44bbd5706a2f60ac6325506f
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-12-19 20:57:37 +0400

    Create (Lecture 4)DivisionFix.cpp

---
+++ (Lecture 4)DivisionFix.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double numberOne = 50;
+    double numberTwo = 7;
+    
+    cout << numberOne/numberTwo << endl;
+    
+    cin.get();
+    return 0;
+    
+}
