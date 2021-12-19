683353a1fb57cc20f5f8e7fdb7dcdd9c6ea1b455
SeanAndrie <84764648+SeanAndrie@users.noreply.github.com>
2021-12-19 22:03:37 +0400

    Create (Lecture 6)Vote.cpp

---
+++ (Lecture 6)Vote.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 16;    
+    
+    if (age >= 18){
+        cout << "YOU CAN VOTE!" << endl;
+    }
+    
+    else {
+        cout << "YOU CANNOT VOTE." << endl;
+    }
+    cin.get();
+}
